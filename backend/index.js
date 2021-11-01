e54ff45a8ca136440388d1715b40a8639d00efd8
Daniel <danielfdoficial@gmail.com>
2021-11-01 18:42:19 -0300

    create: add users route POST

--- backend/index.js
+++ backend/index.js
@@ -3,10 +3,15 @@ const app = express();
 const PORT = 3000;
 
 const tasksRoute = require('./src/Routes/tasksRoute');
+const loginRoute = require('./src/Routes/loginRoute');
+const usersRoute = require('./src/Routes/usersRoute');
+
 
 app.use(express.json());
 
 app.use('/tasks', tasksRoute);
+app.use('/login', loginRoute);
+app.use('/users', usersRoute);
 
 app.get('/', (req, res) => res.send('Hello World!'));
 app.listen(PORT, () => console.log(`Example app listening on PORT ${PORT}`));
--- backend/src/services/helpers.js
+++ backend/src/services/helpers.js
@@ -1,3 +1,5 @@
+const usersModel = require('../models/usersModel');
+
 const validateReqBody = ({ task, status }) => {
   if (typeof task !== 'string' || task.length < 1) return null;
   if (typeof status !== 'string' || status.length < 1) return null;
@@ -5,6 +7,21 @@ const validateReqBody = ({ task, status }) => {
   return true;
 };
 
+const validatePassword = (password) => {
+  if (password.length < 3) return null;
+
+  return true;
+};
+
+const validateUserName = async (userName) => {
+  const userNameExists = await usersModel.getByName(userName);
+  if (userNameExists) return true;
+
+  return null;
+};
+
 module.exports = {
   validateReqBody,
+  validatePassword,
+  validateUserName,
 };
