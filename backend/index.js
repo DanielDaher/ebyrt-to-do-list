0b9477f1efab0b514ead828c292b4f12eec4f560
Daniel <danielfdoficial@gmail.com>
2021-11-01 13:22:12 -0300

    fix: exclude duplicate msc files

--- backend/index.js
+++ backend/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const PORT = 3000;
 
-const tasksRoute = require('./Routes/tasksRoute');
+const tasksRoute = require('./src/Routes/tasksRoute');
 
 app.use(express.json());
 
