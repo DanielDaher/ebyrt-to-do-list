2e86ec256c170d38420065a09a0bdfbbb2a34165
Daniel <danielfdoficial@gmail.com>
2021-11-02 14:48:43 -0300

    create: add useState and submit to loginForm.js

--- frontend/to-do-list/src/pages/homepage/Home.js
+++ frontend/to-do-list/src/pages/homepage/Home.js
@@ -4,9 +4,11 @@ import LoginForm from './components/LoginForm';
 
 export default function Home() {
   return (
-    <div>
-      <h1 className="home-title">To Do List</h1>
-      <p className="home-subtitle">ADD NOTES TO YOUR LIFE!</p>
+    <div className="home-content">
+      <div>
+        <h1 className="home-title">To Do List</h1>
+        <p className="home-subtitle">ADD NOTES TO YOUR LIFE!</p>
+      </div>
       <LoginForm />
     </div>
   );
--- frontend/to-do-list/src/pages/homepage/components/LoginForm.js
+++ frontend/to-do-list/src/pages/homepage/components/LoginForm.js
@@ -1,16 +1,34 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+import '../../../css/LoginForm.css';
 
 export default function LoginForm() {
+  const [user, setUser] = useState('');
+  const [password, setPassword] = useState('');
+
+  const makeLogin = (e) => {
+    e.preventDefault();
+    console.log(user, password);
+  };
+
   return (
-    <form>
+    <form onSubmit={(e) => makeLogin(e)}>
       <label>
         User
-        <input type="text" />
+        <input type="text" onChange={(e) => setUser(e.target.value)} />
       </label>
       <label>
         Password
-        <input type="text" />
+        <input type="text" onChange={(e) => setPassword(e.target.value)} />
       </label>
+      <button
+        type="submit"
+        onSubmit={(e) => makeLogin(e)} >
+        Login
+      </button>
+      <Link to="/characters" className="login-form-link">
+        <p>Not registered yet? Click here!</p>
+      </Link>
     </form>
   );
 };
